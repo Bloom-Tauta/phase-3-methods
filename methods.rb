dec80e90705a484f4ec23adb87c9abc2a459c399
Bloom <bloomtauta@gmail.com>
2023-02-09 01:58:57 +0300

    Second commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name= "Naureen")
+    puts "Hello, #{name}!"
+end
+greet("Jimmy")
+
+def greet_with_default(name="Naureen")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default("Naureen")
+
+
+def add(num1, num2)
+    return num1+num2
+end
+add(2, 5)
+
+def halve(num)
+    puts num
+   end
+    if num = 6
+    return num/2
+    end
+   halve(num)
+   
